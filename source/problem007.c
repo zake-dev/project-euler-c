301b929d1dd20434552ec71be5da04f47c5e86d5
zake-dev <zake-dev@github.com>
2020-08-11 16:05:07 +0900

    misc: edit published date

--- source/problem007.c
+++ source/problem007.c
@@ -1,6 +1,6 @@
 /*******************************************************************************
 Author: Zake Jeonghwan Lee
-Date: 10/08/2020
+Date: 11/08/2020
 
 Problem 7
 By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see 
